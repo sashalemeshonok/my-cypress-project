35de8910ff8039bc06845dc8c955025d372f34b4
aliaksandr.lemiashonak <aliaksandr.lemiashonak@kufar.by>
2025-02-21 12:41:58 +0300

    fix test for geting error

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -2,6 +2,6 @@ describe('visit kufar', () => {
 
   it('open kufar', () => {
     cy.visit('/l')
-    cy.get('[data-testid="user_profile_pic"]').should('not.exist')
+    cy.get('[data-testid="user_profile_pic"]').should('exist')
   })
 })
