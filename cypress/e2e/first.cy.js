995d1cd3efedc2de5a6e3c4e016eb23f7441b40e
aliaksandr.lemiashonak <aliaksandr.lemiashonak@kufar.by>
2025-03-31 11:09:00 +0300

    add wf file for running pm collection

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -2,6 +2,6 @@ describe('visit kufar', () => {
 
   it('open kufar', () => {
     cy.visit('/l')
-    cy.get('[data-testid="user_profile_pic"]').should('exist')
+    cy.get('[data-testid="user_profile_pic"]', { timeout: 10000 }).should('exist')
   })
 })
--- cypress/e2e/second.cy.js
+++ cypress/e2e/second.cy.js
@@ -1,6 +1,7 @@
 describe('visit auto kufar', () => {
 
     it('open auto kufar', () => {
-      cy.visit('https://auto.kufar.by/')
+      cy.visit('/')
+      cy.get('[data-testid="user_profile_pic"]', { timeout: 10000 }).should('exist')
     })
   })
